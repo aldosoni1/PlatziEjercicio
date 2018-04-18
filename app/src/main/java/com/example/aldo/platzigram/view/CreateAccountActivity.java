94824916af981037a2564f38c0fd2dc870d5ae0c
Aldo <aldotesa96@gmail.com>
2018-04-17 23:05:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/aldo/platzigram/view/CreateAccountActivity.java
@@ -0,0 +1,26 @@
+package com.example.aldo.platzigram.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.example.aldo.platzigram.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+        showToolbar(getResources().getString(R.string.toolbar_tittle_create_account), true);
+
+    }
+
+    public void showToolbar(String tittle, boolean upButton){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(tittle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+
+    }
+}
